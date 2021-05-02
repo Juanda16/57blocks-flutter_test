2353855873c33da684193ff69b359657814147b4
Juan Arismendy <juand16@gmail.com>
2021-05-02 13:08:06 -0500

    Like working in statefulWidget

--- lib/list_view/domain/entities/video_entity.dart
+++ lib/list_view/domain/entities/video_entity.dart
@@ -3,7 +3,7 @@ import 'package:equatable/equatable.dart';
 class VideoEntity extends Equatable {
   //dynamic thumbnail;
   final String id, title, description, duration;
-  final bool like;
+  bool like;
   final String thumbnailsUrl;
 
   VideoEntity(
@@ -14,6 +14,10 @@ class VideoEntity extends Equatable {
       this.like = false,
       this.thumbnailsUrl =
           "https://yachteskills.com/wp-content/themes/yacht/images/video-thumbnail-default.png"});
+
+  changeLike() {
+    this.like = !like;
+  }
 
   @override
   List<Object?> get props => [id];
--- lib/list_view/presentation/widgets/video_tail.dart
+++ lib/list_view/presentation/widgets/video_tail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:like_button/like_button.dart';
 import 'package:my_video_app/list_view/domain/entities/video_entity.dart';
 import 'package:my_video_app/list_view/presentation/pages/video_detail_page.dart';
 
@@ -12,7 +13,7 @@ class VideoTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var like = video.like;
+    //bool like = widget.video.like;
     return Card(
       child: Column(
         children: [
@@ -21,13 +22,6 @@ class VideoTile extends StatelessWidget {
               Navigator.pushNamed(context, VideoDetailPage.id,
                   arguments: ScreenArguments(videoDetail: video));
             },
-            // onTap: () => Navigator.push(
-            //     context,
-            //     MaterialPageRoute(
-            //         builder: (context) => VideoDetailPage(
-            //               video: video,
-            //             ))),
-
             child: Container(
               // Third of the size of the screen
               height: MediaQuery.of(context).size.height / 4,
@@ -40,22 +34,24 @@ class VideoTile extends StatelessWidget {
             title: Text(video.id),
             subtitle: Text(video.title),
 
-            // trailing: GestureDetector(
-            //   child: Icon(
-            //     // NEW from here...
-            //     like ? Icons.favorite : Icons.favorite_border,
-            //     color: like ? Colors.red : null,
-            //   ),
-            //   onTap: () {
-            //     // setState(() {
-            //     //   if (like) {
-            //     //     print('tapping');
-            //     //     like = false;
-            //     //   } else {
-            //     //     like = true;
-            //     //   }
-            //     });
-            //},
+            //trailing: LikeButton(),
+            trailing: Column(
+              children: [
+                Container(
+                  child: IconButton(
+                    icon: Icon(
+                      video.like ? Icons.favorite : Icons.favorite_border,
+                      color: video.like ? Colors.red : null,
+                    ),
+                    onPressed: () {
+                      // setState(() {
+                      video.like = !video.like;
+                      // });
+                    },
+                  ),
+                )
+              ],
+            ),
           ),
         ],
       ),
@@ -66,3 +62,31 @@ class VideoTile extends StatelessWidget {
 // onTap: () {
 // Navigator.pushNamed(context, VideoDetailPage.id);
 // },
+
+// trailing: GestureDetector(
+//   child: Icon(
+//     // NEW from here...
+//     like ? Icons.favorite : Icons.favorite_border,
+//     color: like ? Colors.red : null,
+//   ),
+//   onTap: () {
+//     // setState(() {
+//     //   if (like) {
+//     //     print('tapping');
+//     //     like = false;
+//     //   } else {
+//     //     like = true;
+//     //   }
+//     });
+//},
+
+// onTap: () => Navigator.push(
+//     context,
+//     MaterialPageRoute(
+//         builder: (context) => VideoDetailPage(
+//               video: video,
+//             ))),
+// trailing: Icon(
+//   like ? Icons.favorite : Icons.favorite_border,
+//   color: like ? Colors.red : null,
+// ),
